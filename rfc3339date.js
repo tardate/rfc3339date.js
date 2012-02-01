6e9c97ad52718dd32a82ab564766ff97832cc14b
Paul Gallagher <gallagher.paul@gmail.com>
2012-02-01 09:17:29 +0800

    GH#3 Merge month rollover bug fix

--- rfc3339date.js
+++ rfc3339date.js
@@ -1,8 +1,8 @@
 /*
- * rfc3339date.js
+ * rfc3339date.js version 0.1.3
  *
  * Adds ISO 8601 / RFC 3339 date parsing to the Javascript Date object.
- * Usage: 
+ * Usage:
  *   var d = Date.parseISO8601( "2010-07-20T15:00:00Z" );
  *   var d = Date.parse( "2010-07-20T15:00:00Z" );
  * Tested for compatibilty/coexistence with:
@@ -12,12 +12,12 @@
  * Copyright (c) 2010 Paul GALLAGHER http://tardate.com
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  */
 
-/* 
+/*
  * Number.prototype.toPaddedString
- * Number instance method used to left-pad numbers to the specified length 
+ * Number instance method used to left-pad numbers to the specified length
  * Used by the Date.prototype.toRFC3339XXX methods
  */
 Number.prototype.toPaddedString = function(len , fillchar) {
@@ -27,10 +27,10 @@ Number.prototype.toPaddedString = function(len , fillchar) {
   return result;
 }
 
-/* 
+/*
  * Date.prototype.toRFC3339UTCString
  * Date instance method to format the date as ISO8601 / RFC 3339 string (in UTC format).
- * Usage: var d = new Date().toRFC3339UTCString(); 
+ * Usage: var d = new Date().toRFC3339UTCString();
  *              => "2010-07-25T11:51:31.427Z"
  * Parameters:
  *  supressFormating : if supplied and 'true', will force to remove date/time separators
@@ -49,10 +49,10 @@ Date.prototype.toRFC3339UTCString = function(supressFormating , supressMillis){
   return result + 'Z';
 }
 
-/* 
+/*
  * Date.prototype.toRFC3339LocaleString
  * Date instance method to format the date as ISO8601 / RFC 3339 string (in local timezone format).
- * Usage: var d = new Date().toRFC3339LocaleString(); 
+ * Usage: var d = new Date().toRFC3339LocaleString();
  *              => "2010-07-25T19:51:31.427+08:00"
  * Parameters:
  *  supressFormating : if supplied and 'true', will force to remove date/time separators
@@ -75,10 +75,10 @@ Date.prototype.toRFC3339LocaleString = function(supressFormating , supressMillis
   return result;
 }
 
-/* 
+/*
  * Date.parseRFC3339
  * extend Date with a method parsing ISO8601 / RFC 3339 date strings.
- * Usage: var d = Date.parseRFC3339( "2010-07-20T15:00:00Z" ); 
+ * Usage: var d = Date.parseRFC3339( "2010-07-20T15:00:00Z" );
  */
 Date.parseRFC3339 = function(dString){
   if (typeof dString != 'string') return;
@@ -103,7 +103,7 @@ Date.parseRFC3339 = function(dString){
       result.setUTCSeconds(secs);
       result.setUTCMilliseconds(millis);
       if (d[13] && d[14]) {
-        var offset = (d[15] * 60) 
+        var offset = (d[15] * 60)
         if (d[17]) offset += parseInt(d[17],10);
         offset *= ((d[14] == '-') ? -1 : 1);
         result.setTime(result.getTime() - offset * 60 * 1000);
@@ -115,11 +115,11 @@ Date.parseRFC3339 = function(dString){
   return result;
 };
 
-/* 
+/*
  * Date.parse
  * extend Date with a parse method alias for parseRFC3339.
  * If parse is already defined, chain methods to include parseRFC3339
- * Usage: var d = Date.parse( "2010-07-20T15:00:00Z" ); 
+ * Usage: var d = Date.parse( "2010-07-20T15:00:00Z" );
  */
 if (typeof Date.parse != 'function') {
   Date.parse = Date.parseRFC3339;
